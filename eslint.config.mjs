a618fcc2382163ca38cc693a5eb1183c0377ecd0
Joélisson Santana <joelisson997@gmail.com>
2024-05-25 19:39:52 -0300

    feat: configurado projeto inicial

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default[
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+];
